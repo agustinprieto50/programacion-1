734075616e8cf05a0adc8e0e4633381b0512792c
MatiasTomasRobbio <matirobbio@hotmail.com>
2022-09-27 18:35:16 -0300

    bootstrap

--- frontend/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import os
-from dotenv import load_dotenv
-load_dotenv()
-app = Flask(__name__)
-if __name__ == '__main__':
-	app.run(port=os.getenv("PORT"),debug=True)
-#comentario
-#otro comentario
