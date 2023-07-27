39543c08f1990d4d0cf7bbdc78a8dff31a335a27
MatiasTomasRobbio <matirobbio@hotmail.com>
2023-07-27 19:19:14 -0300

    delete user try

--- frontend/src/src/app/components/users-table/users-table.component.ts
+++ frontend/src/src/app/components/users-table/users-table.component.ts
@@ -2,6 +2,8 @@ import { AnimateTimings } from '@angular/animations';
 import { Token } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
 import { GetUsersService } from 'src/app/services/get-users.service';
+import { DeleteUserService } from 'src/app/services/delete-user.service'; 
+
 
 @Component({
   selector: 'app-users-table',
@@ -11,7 +13,7 @@ import { GetUsersService } from 'src/app/services/get-users.service';
 export class UsersTableComponent implements OnInit {
   @Input() parameters!:string;
   users:any;
-  constructor(private getUsers: GetUsersService) { }
+  constructor(private getUsers: GetUsersService,private deleteUserService: DeleteUserService) { }
 
   ngOnInit(): void {
     this.getUsers.getUsersAll('').subscribe((data:any)=> {
@@ -19,6 +21,39 @@ export class UsersTableComponent implements OnInit {
       console.log(this.users)
   })
     
+  }
+
+  openConfirmationModal(): void {
+    const confirmationModal = document.getElementById('confirmationModal');
+    confirmationModal?.classList.add('show');
+    confirmationModal?.setAttribute('aria-modal', 'true');
+    confirmationModal?.setAttribute('style', 'display: block');
+  }
+
+  closeConfirmationModal(): void {
+    const confirmationModal = document.getElementById('confirmationModal');
+    confirmationModal?.classList.remove('show');
+    confirmationModal?.setAttribute('aria-modal', 'false');
+    confirmationModal?.setAttribute('style', 'display: none');
+  }
+
+
+  deleteUser(userId: any): void {
+    this.deleteUserService.deleteUser(userId).subscribe(
+      
+      () => {
+        console.log(`User with ID ${userId} deleted successfully.`);
+      },
+      (error) => {
+        console.log(`Tried deleting user ${userId}`);
+        console.error(`Error deleting user with ID ${userId}: ${error}`);
+      }
+    );
+  }
+
+  deleteUserConfirmed(userId:any): void {
+    this.deleteUser(userId);
+    this.closeConfirmationModal();
   }
 
 }
